6ebd911180a6a6aa3e8632665649a1c1002a7502
LinaFrancoV <147664296+LinaFrancoV@users.noreply.github.com>
2023-11-01 13:20:13 -0500

    Add files via upload
    cambios de modularizacion

--- fecha-de-nacimiento.swift
+++ fecha-de-nacimiento.swift
@@ -3,47 +3,29 @@ import Foundation
 print("Ingresa tu fecha de nacimiento (en el formato YYYY-MM-DD):")
 let fechaNacimiento = readLine()
 
-func calcularEdad(fechaNacimiento: String) -> Int? {
-    let dateFormatter = FormatoFecha()
-    if let fechaNacimientoDate = dateFormatter.date(from: fechaNacimiento) {
-        let calendar = Calendar.current
-        let ageComponents = calendar.dateComponents([.year], from: fechaNacimientoDate, to: Date())
-        let edad = ageComponents.year
-        return edad
-    } else {
-        print("La fecha de nacimiento ingresada no es válida.")
-        return nil
-    }
+func convertirFecha(fechaNacimiento: String) -> Date? {
+    let dateFormatter = formatearFecha()
+    return dateFormatter.date(from: fechaNacimiento)
 }
 
-func FormatoFecha() -> DateFormatter {
+func formatearFecha() -> DateFormatter {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd"
     return dateFormatter
 }
 
-func calcularDiaSemana(fechaNacimiento: String) -> String? {
-    let dateFormatter = FormatoFecha()
-    if let fechaNacimientoDate = dateFormatter.date(from: fechaNacimiento) {
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.weekday], from: fechaNacimientoDate)
-        if let weekday = components.weekday {
-            let weekdayNames = dateFormatter.weekdaySymbols
-            return weekdayNames[weekday - 1]
-        } else {
-            return "Día de la semana no disponible."
-        }
-    } else {
-        print("La fecha de nacimiento ingresada no es válida.")
-        return nil
-    }
+func calcularEdad(fechaNacimiento: Date) -> Int? {
+    let calendar = Calendar.current
+    let ageComponents = calendar.dateComponents([.year], from: fechaNacimiento, to: Date())
+    let edad = ageComponents.year
+    return edad
 }
 
 func imprimirInformacion() {
-    if let fechaNacimiento = fechaNacimiento,
-        let edad = calcularEdad(fechaNacimiento: fechaNacimiento),
-        let diaSemana = calcularDiaSemana(fechaNacimiento: fechaNacimiento) {
-        print("Tu fecha de nacimiento es un \(diaSemana). Tienes \(edad) años.")
+    if let fechaNacimiento = fechaNacimiento, let fechaConvertida = convertirFecha(fechaNacimiento: fechaNacimiento) {
+        if let edad = calcularEdad(fechaNacimiento: fechaConvertida) {
+            print("Tienes \(edad) años.")
+        }
     }
 }
 
